5f5cd9b4961099538c0b506df7a7f0c4d20b56a4
Fabrício Barbosa Viegas <incy8498@gmail.com>
2026-01-12 20:03:37 -0300

    Add Clarity tracking script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,13 @@ export default function RootLayout({
             gtag('config', 'AW-17848976773');
           `}
         </Script>
+        <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "v0il0gc5si");
+</script>
       </head>
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable} min-h-screen flex flex-col bg-background text-foreground`}>
         <Header />
